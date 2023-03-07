54f1573534943897dd11aea77b7ba5617078af80
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-03-08 04:07:37 +0600

    initial commit

---
+++ src/pages/about_us.js
@@ -0,0 +1,10 @@
+
+const about_us = () => {
+    return (
+        <div>
+            <h1>This is About Us Page.</h1>
+        </div>
+    );
+};
+
+export default about_us;
---
+++ src/pages/packages.js
@@ -0,0 +1,10 @@
+
+const Packages = () => {
+  return (
+    <div>
+      <h1>This is package page. </h1>
+    </div>
+  );
+};
+
+export default Packages;
